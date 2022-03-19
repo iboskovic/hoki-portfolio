a32bdbb53d679a05c74d704cc21e862b7b2d43f3
Ivan Boskovic <ivan7vkci7@gmail.com>
2022-03-19 11:11:22 +0100

    Project setup
    adding necessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
-import './App.css';
+import '../src/styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="type--wgt--bold">This is a font test</div>
+      <div className="type--wgt--extra-bold">This is a font test</div>
+      <div className="type--wgt--regular">This is a font test</div>
+      <div className="type--wgt--thin-italic">This is a font test</div>
+      <a href="#">Test</a>
     </div>
   );
 }
