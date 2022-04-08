37fbc68dabd49de6aed17f1a273cffdda457d6f8
Ivan Boskovic <ivan7vkci7@gmail.com>
2022-04-08 19:58:49 +0200

    form button

--- src/app/pages/landing/components/LayoutMid.tsx
+++ src/app/pages/landing/components/LayoutMid.tsx
@@ -7,19 +7,20 @@ const LayoutMid = () => {
                 <a href="#contact" className="type--color--primary type--uppercase">Contact</a>
             </div>
             <div className="m--t-262 title">
-                <div className="type--lg type--wgt--extra-bold type--color--primary">Design or <span className="type--color--secondary type--wgt--light-italic">die</span>.</div>
-                <div className="type--wgt--regular type--sm type--color--secondary m--l-16 m--t-16">Don't</div>
+                <div className="type--lg type--wgt--extra-bold type--color--primary">Lorem <span className="type--color--primary type--wgt--light-italic">ipsum.</span></div>
+                <div className="type--wgt--regular type--sm type--color--secondary m--t-16">No need for cringey quotes.</div>
                 <div className="title__image">
                 </div>
             </div>
 
+            <div id="about"></div>
             <div className="m--t-472 m--l-16 about" id="about">
                 <div className="w--734">
-                    <div className="type--base type--color--secondary type--wgt--regular m--b-40">I’m 27-Year-old creative and enthusiastic designer based in Osijek Croatia and working in small tech agency.</div>
+                    <div className="type--base-plus type--color--secondary type--wgt--regular m--b-40">I’m 27-Year-old creative and enthusiastic designer based in Osijek Croatia and working in small tech agency.</div>
 
-                    <div className="type--base type--color--secondary type--wgt--regular m--b-40">I’m in love with what I do so it never stops. Im always open for some cool ideas and projects. As freelancer, I work with companies, agencies, startups or individuals.</div>
+                    <div className="type--base-plus type--color--secondary type--wgt--regular m--b-40">I’m in love with what I do so it never stops. Im always open for some cool ideas and projects. As freelancer, I work with companies, agencies, startups or individuals.</div>
 
-                    <div className="type--base type--color--secondary type--wgt--regular">Skilled in combining user needs and expectations with client requirements in order to deliver high quality products following UI patterns and best practices.</div>
+                    <div className="type--base-plus type--color--secondary type--wgt--regular">Skilled in combining user needs and expectations with client requirements in order to deliver high quality products following UI patterns and best practices.</div>
                 </div>
                 <div className="about__image">
                     {/* <img className='title__image' src={hokiTitle} alt="hoki-title" /> */}
@@ -28,24 +29,24 @@ const LayoutMid = () => {
 
             <div className="m--t-242" id="projects">
                 <div className="project">
-                    <div className="type--lg type--color--primary type--wgt--bold">Releese</div>
-                    <div>
+                    <div className="type--lg type--color--primary type--wgt--bold">Mental <span className="type--wgt--regular-italic type--color--primary">Health</span></div>
+                    <div className="w--230">
                         <div className="type--base type--color--secondary type--wgt--bold m--b-8">Lead UX/UI Designer</div>
                         <div className="type--base type--color--secondary type--wgt--regular">Mobile app</div>
                     </div>
                 </div>
 
                 <div className="project">
-                    <div className="type--lg type--color--primary type--wgt--bold">Jedemi.<span className="type--wgt--light-italic type--color--primary">se</span></div>
-                    <div>
+                    <div className="type--lg type--color--primary type--wgt--bold">Only<span className="type--wgt--regular-italic type--color--primary">Bangers</span></div>
+                    <div className="w--230">
                         <div className="type--base type--color--secondary type--wgt--bold m--b-8">Lead UX/UI Designer</div>
-                        <div className="type--base type--color--secondary type--wgt--regular">Web app</div>
+                        <div className="type--base type--color--secondary type--wgt--regular">Mobile app</div>
                     </div>
                 </div>
 
                 <div className="project">
-                    <div className="type--lg type--color--primary type--wgt--bold">Optika <span className="type--wgt--light-italic type--color--primary">Anda</span></div>
-                    <div>
+                    <div className="type--lg type--color--primary type--wgt--bold">Optika <span className="type--wgt--regular-italic type--color--primary">Anda</span></div>
+                    <div className="w--230">
                         <div className="type--base type--color--secondary type--wgt--bold m--b-8">UX/UI Designer</div>
                         <div className="type--base type--color--secondary type--wgt--regular">Website</div>
                     </div>
@@ -53,19 +54,30 @@ const LayoutMid = () => {
 
                 <div className="project">
                     <div className="type--lg type--color--primary type--wgt--bold">GPŠ</div>
-                    <div>
+                    <div className="w--230">
                         <div className="type--base type--color--secondary type--wgt--bold m--b-8">Lead UX/UI Designer</div>
                         <div className="type--base type--color--secondary type--wgt--regular">Website</div>
                     </div>
                 </div>
 
                 <div className="project">
-                    <div className="type--lg type--color--primary type--wgt--bold">Olive <span className="type--wgt--light-italic type--color--primary">oil</span></div>
-                    <div>
+                    <div className="type--lg type--color--primary type--wgt--bold">Olive <span className="type--wgt--regular-italic type--color--primary">oil</span></div>
+                    <div className="w--230">
                         <div className="type--base type--color--secondary type--wgt--bold m--b-8">Lead UX/UI Designer</div>
                         <div className="type--base type--color--secondary type--wgt--regular">Website</div>
                     </div>
                 </div>
+            </div>
+
+            <div className="contact" id="contact">
+                <div className="type--md type--wgt--extra-bold type--color--primary m--b-64">Did we just become <span className="type--color--primary type--wgt--light-italic">friends?</span></div>
+                <button className="btn btn--primary btn--base">Let's talk</button>
+            </div>
+
+            <div className="l__nav">
+                <a href="#projects" className="type--color--primary type--uppercase">Projects</a>
+                <a href="#about" className="type--color--primary type--uppercase">About</a>
+                <a href="#contact" className="type--color--primary type--uppercase">Contact</a>
             </div>
         </div >
     )
